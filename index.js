c33a24b4255b80b8f0b4b5946c39ad2359591092
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-09-26 21:19:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+ * Created by gaoyoujian on 2019/9/26.
+ */
+var studentScore={
+    score:'',
+    init:function () {
+        //alert('init');
+        this.getScore();
+    },
+    getScore:function () {
+        var scoreValue = document.getElementById('studentScore');
+        var scoreBtn = document.getElementById('checkScore');
+        scoreBtn.onclick = function () {
+            studentScore.score = scoreValue.value;
+            if(studentScore.score == '') {
+                //尚未输入
+                alert('你尚未输入成绩，请输入!');
+                return;
+            }else{
+                studentScore.calculate();
+            }
+        }
+    },
+    calculate:function () {
+        var score = studentScore.score;
+        switch (true) {
+            case score>90 && score<=100:
+                 studentScore._finalScore('优等生');
+                 break;
+            case score>80 && score<=90:
+                studentScore._finalScore('一等生');
+                break;
+            case score>70 && score<=80:
+                studentScore._finalScore('二等生');
+                break;
+            case score>60 && score<=70:
+                studentScore._finalScore('三等生');
+                break;
+            default:
+                alert("请输入1-100的数字");
+                break;
+        }
+    },
+    _finalScore:function (str) {
+        var finalScore = document.getElementById('finalScore');
+        finalScore.innerHTML = str;
+    }
+};
+(function() {
+    studentScore.init();
+})();
