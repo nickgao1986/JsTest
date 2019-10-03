b710c4a69a553bb4e4bd70a6aa1964f5337851a9
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-10-03 12:04:45 +0800

    fix bug

--- hospital/hospital.js
+++ hospital/hospital.js
@@ -3,7 +3,6 @@
  */
 var studentScore={
     init:function () {
-        console.log("ddd");
         $(document).ready(function () {
             console.log("bbbb");
             $(".sub-close").click(function () {
--- nav/nav.js
+++ nav/nav.js
@@ -1,14 +1,1 @@
-function  nav(liID,ulID) {
-    var oLi = document.getElementById(liID);
-    var oUl = document.getElementById(ulID);
-
-    oLi.onmouseover = function () {
-        oUl.style.height = '130px';
-    };
-    oLi.onmouseout = function () {
-        oUl.style.height = '0';
-    }
-}
-nav('li1','ul1');
-nav('li2','ul2');
-nav('li3','ul3');
+console.log("bbb");
