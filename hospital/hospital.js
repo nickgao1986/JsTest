313cf10bdb5057b8eced576398bfc0e3e39d700e
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-10-03 11:45:49 +0800

    fix bug

--- hospital/hospital.js
+++ hospital/hospital.js
@@ -2,8 +2,18 @@
  * Created by gaoyoujian on 2019/9/26.
  */
 var studentScore={
-
+    init:function () {
+        console.log("ddd");
+        $(document).ready(function () {
+            console.log("bbbb");
+            $(".sub-close").click(function () {
+                console.log("aaaa");
+                alert("class="+$(".sub-close").text());
+            });
+        });
+    }
 };
 (function() {
+    console.log("ccc");
     studentScore.init();
 })();
