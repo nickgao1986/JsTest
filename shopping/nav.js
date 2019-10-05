0684d5260725ba6e343be9459970cdbe08e88f4b
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-10-05 12:47:45 +0800

    fix bug

---
+++ shopping/nav.js
@@ -0,0 +1,11 @@
+$(function() {
+    console.log("aaaaa");
+    var $slider     = $('.banner').unslider({
+        dots: true
+    });
+    // 前一张和后一张操作的事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    });
+});
