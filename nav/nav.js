03f1586da7848a3d8ca4a50113a6c60956aaf85a
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-10-06 10:15:42 +0800

    fix bug

--- nav/nav.js
+++ nav/nav.js
@@ -1,3 +1,17 @@
 $(function() {
-    console.log("cccc");
+    function  nav(liID,ulID) {
+        var oLi = document.getElementById(liID);
+        var oUl = document.getElementById(ulID);
+
+        oLi.onmouseover = function () {
+            oUl.style.height = '130px';
+        };
+        oLi.onmouseout = function () {
+            oUl.style.height = '0';
+        }
+    }
+    nav('li1','ul1');
+    nav('li2','ul2');
+    nav('li3','ul3');
+
 });
